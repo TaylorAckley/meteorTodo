4ec843854377f2f6272638b3fe1c9627e3f52a4c
TaylorAckley <taylorackley@gmail.com>
2015-07-29 18:13:18 -0700

    refactor html

--- meteortodo/server/accounts.js
+++ meteortodo/server/accounts.js
@@ -1,11 +1,3 @@
-Accounts.onCreateUser(function(options, user) {
-    if (options.profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
-        user.profile = options.profile;
-    }
-    return user;
-});
-
-Accounts.config({
-    sendVerificationEmail: true
-});
+//Accounts.config({
+  //  sendVerificationEmail: true
+//});
